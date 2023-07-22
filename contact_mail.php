08c32058ba35dee1d57f501a67a276719f0200f0
Cristian Osorio <cosoriogut@gmail.com>
2023-07-22 14:29:08 -0400

    up
    up

--- contact_mail.php
+++ contact_mail.php
@@ -1,63 +1,70 @@
-
 <?php
-#se agrega phpmailer y smtp
+//SE REQUIERE PHPMAILES + SMTP
 require("class.phpmailer.php");
 require("class.smtp.php");
 
+$mail = new PHPMailer();
+$mail->isSMTP();
+
+// SE ACTIVA LA CODIFICACION UTF-8 PARA SIGNOS
+$mail->CharSet = 'UTF-8';
+
+// SE INGRESA EL SERVIDOR SMTP Y EL PUERTO 465 PARA GMAIL ES EL 587
+$mail->Host = "smtp.gmail.com";
+$mail->Port = 587;
+$mail->SMTPAuth = true;
+$mail->SMTPSecure = true;
+
+// INGRESAR CORREO Y PASSWORD
+$mail->Username = 'criosoguttest@gmail.com';
+$mail->Password = 'aleumyagctlojxhk';
+
+// CORREO ELECTRONICO DE: CONTACTO@BLA...
+$mail->From = 'criosoguttest@gmail.com';
+// TITULO DEL CORREO A RECIBIR
+$mail->FromName = "Pan Gourmet Chile - Cotización";
+//
+$mail->AddAddress('criosoguttest@gmail.com', "PanGourmet");
+//$mail->AddAddress($_POST['email']);
+// CON COPIA
+//$mail->addCC('correo_x@gmail.com');
+// CON COPIA OCULTA
+//$mail->addBCC('correo_x@hotmail.cl');
+
+$mail->WordWrap = 50;
+$mail->IsHTML(true);
+
+//TITULO DEL CORREO A RECIBIR (ASUNTO)
+$mail->Subject = "Cotización";
+//CUERPO DEL CORREO DESDE EL NOMBRE HASTA EL MENSAJE
+$mail->Body    = <<<EOT
+Nombre:	{$_POST['name']}<br/>
+Correo: {$_POST['email']}<br/>
+Teléfono: {$_POST['phone']}<br/>
+Asunto: {$_POST['subject']}<br/>
+Mensaje: {$_POST['message']}<br/>
+EOT;
 
 
+$mail->SMTPOptions = array(
+'ssl' => array(
+'verify_peer' => false,
+'verify_peer_name' => false,
+'allow_self_signed' => true
+)
+);
 
-    if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
-        # FIX: Replace this email with recipient email
-        $mail_to = "criosoguttest@gmail.com";
-        
-        
+if(!$mail->Send())
 
-        # Sender Data
-        $subject = trim($_POST["subject"]);
-        $name = str_replace(array("\r","\n"),array(" "," ") , 
-        strip_tags(trim($_POST["name"])));
-        $email = filter_var(trim($_POST["email"]), FILTER_SANITIZE_EMAIL);
-        $phone = trim($_POST["phone"]);
-        $message = trim($_POST["message"]);
+{
+   echo "Error al enviar. <p>";
+   echo "Mailer Error: " . $mail->ErrorInfo;
+   exit;
+}
+//MENSAJE PARA EL CLIENTE INDICANDO MENSAJE ENVIADO
+echo "Mensaje enviado!";
 
-        if ( empty($name) OR !filter_var($email, FILTER_VALIDATE_EMAIL) OR empty($phone) OR empty($subject) OR empty($message)) {
-            # Set a 400 (bad request) response code and exit.
-            http_response_code(400);
-            echo "Por favor. Complete el formulario y vuelva a intentarlo.";
-            exit;
-        }
 
-        # Mail Content
-        $content = "Name: $name\n";
-        $content .= "Email: $email\n\n";
-        $content .= "Phone: $phone\n";
-        $content .= "Message:\n$message\n";
-
-        # email headers.
-        $headers  = "MIME-Version: 1.0" . "\r\n";
-        $headers .= "Content-type: text/html; charset=iso-8859-1" . "\r\n";
-        $headers .= "From: ". $email. "\r\n";
-        $headers .= "Reply-To: ". $mail_to. "\r\n";
-        $headers .= "X-Mailer: PHP/" . phpversion();
-        $headers .= "X-Priority: 1" . "\r\n"; 
-
-        # Send the email.
-        $success = mail($mail_to, $subject, $content, $headers);
-        if ($success) {
-            # Set a 200 (okay) response code.
-            http_response_code(200);
-            echo "¡Gracias! Tu mensaje ha sido enviado.";
-        } else {
-            # Set a 500 (internal server error) response code.
-            http_response_code(500);
-            echo "¡Ups! Algo salió mal, no pudimos enviar su mensaje.";
-        }
-
-        } else {
-            # Not a POST request, set a 403 (forbidden) response code.
-            http_response_code(403);
-            echo "Hubo un problema con su envío, inténtelo de nuevo.";
-        }
-?>
+	
+?> 
