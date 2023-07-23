0dde80590e5d1e184e537385ce9bf6e4683c9279
Cristian Osorio <cosoriogut@gmail.com>
2023-07-22 20:32:44 -0400

    update formulario funcional
    formulario de contacto funcional, recepcion de correo OK.

--- contact_mail.php
+++ contact_mail.php
@@ -21,10 +21,11 @@ try {
     //Server settings
     $mail->SMTPDebug = 0;                      //Enable verbose debug output
     $mail->isSMTP();                                            //Send using SMTP
+    $mail->CharSet = 'UTF-8'; // se habilita signos UTF-8
     $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
     $mail->Username   = 'criosoguttest@gmail.com';                     //SMTP username
-    $mail->Password   = 'aleumyagctlojxhk';                               //SMTP password
+    $mail->Password   = 'aleumyagctlojxhk'; // se habilita contraseña de aplicacion en gmail, contraseña temporal SMTP correoSMTPcontacto                               //SMTP password
     $mail->SMTPSecure = 'tls';            //Enable implicit TLS encryption
     $mail->Port       = 587;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
