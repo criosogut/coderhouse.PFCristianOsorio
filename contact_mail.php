560e93dcfb5c47f33a7088878b7cf583cf604ca6
criosogut <135785567+criosogut@users.noreply.github.com>
2023-07-24 23:11:48 -0400

    Update contact_mail.php

--- contact_mail.php
+++ contact_mail.php
@@ -5,6 +5,45 @@ use PHPMailer\PHPMailer\Exception;
 require 'phpmailer/Exception.php';
 require 'phpmailer/PHPMailer.php';
 require 'phpmailer/SMTP.php';
+// Función para enviar la respuesta automática
+function enviarRespuestaAutomatica($email, $name) {
+    $mail = new PHPMailer(true);
+    try {
+        //Server settings
+        $mail->SMTPDebug = 0;                           //Enable verbose debug output
+        $mail->isSMTP();                                //Send using SMTP
+        $mail->CharSet = 'UTF-8';                       // se habilita signos UTF-8
+        $mail->Host       = 'smtp.gmail.com';           //Set the SMTP server to send through
+        $mail->SMTPAuth   = true;                       //Enable SMTP authentication
+        $mail->Username   = 'criosoguttest@gmail.com';  //SMTP username
+        $mail->Password   = 'aleumyagctlojxhk';         // se habilita contraseña de aplicacion en gmail, contraseña temporal SMTP correoSMTPcontacto, no utilizar contraseña de acceso de gmail
+        $mail->SMTPSecure = 'tls';                      //Enable implicit TLS encryption
+        $mail->Port       = 587;                        //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+        //Recipients
+        $mail->setFrom('criosoguttest@gmail.com', 'Cotizacion');
+        $mail->addAddress($email, $name);
+        //Content
+        $mail->isHTML(true); 
+        //Set email format to HTML
+        $mail->Subject = 'Respuesta automática - Cotización';
+        $mail->Body    = <<<EOT
+        Hola {$name},:<br/>
+        Esta es una respuesta automática.
+        <br/>
+        Gracias por contactarnos!
+        <br/>
+        Hemos recibido tu mensaje y nos pondremos en contacto contigo en las próximas 24 horas. <br/>
+        <br/>
+        Saludos,<br/>
+        Equipo Pan Gourmet Chile
+        EOT;
+        $mail->send();
+        return true;
+    } catch (Exception $e) {
+        return false;
+    }
+}
+//enviar correo original
 $mail = new PHPMailer(true);
 try {
     //Server settings
@@ -20,7 +59,10 @@ try {
     //Recipients
     $mail->setFrom('criosoguttest@gmail.com', 'Cotizacion');
     $mail->addAddress('criosoguttest@gmail.com');   //Add a recipient
-    $mail->addCC('cosoriogut@gmail.com');           // con copia
+    //Reply to (Respuesta automática)
+    $replyEmail = $_POST['email'];
+    $replyName = $_POST['name'];
+    $mail->addReplyTo($replyEmail, $replyName);
     //Content
     $mail->isHTML(true); 
     //Set email format to HTML
@@ -33,7 +75,9 @@ try {
     Mensaje: {$_POST['message']}<br/>
     EOT;
     $mail->send();
-    echo 'Mensaje enviado!';
+    // Envío de la respuesta automática
+    enviarRespuestaAutomatica($replyEmail, $replyName);
+    echo 'Mensaje enviado!... favor revisa tu bandeja de entrada';
 } catch (Exception $e) {
     echo "Error al enviar... {$mail->ErrorInfo}";
 }
